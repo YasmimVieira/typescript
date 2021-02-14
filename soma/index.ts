0207d9291df5f0155ef985d36505430280078bfa
YasmimVieira <vieiray937@gmail.com>
2021-02-13 22:29:37 -0300

    chore: example sum

---
+++ soma/index.ts
@@ -0,0 +1,11 @@
+const input1 = document.querySelector('[data-input1]') as HTMLInputElement
+const input2 = document.querySelector('[data-input2]') as HTMLInputElement
+const button = document.querySelector('[data-button]')
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+button.addEventListener('click', function() {
+    console.log(Number(input1.value), Number(input2.value));
+})
